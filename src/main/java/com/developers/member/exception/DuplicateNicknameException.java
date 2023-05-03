a0c32cc8470843d88965078895440b8e09ad9a25
lango <xmun777@naver.com>
2023-05-03 15:00:36 +0900

    Update README.md

--- src/main/java/com/developers/member/exception/DuplicateNicknameException.java
+++
@@ -1,7 +0,0 @@
-package com.developers.member.exception;
-
-public class DuplicateNicknameException extends RuntimeException {
-    public DuplicateNicknameException(String message) {
-        super(message);
-    }
-}
